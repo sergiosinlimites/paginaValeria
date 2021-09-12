91b8b8e1b98ae1f8fb4bce8fd393c2788cef4146
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2021-09-12 02:16:03 -0500

    nuevo

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function animacionAbajo() {
     let scrollTop = document.documentElement.scrollTop;
     for(let i = 0; i < animadoAbajo.length; i++){
         let alturaElemento = animadoAbajo[i].offsetTop;
-        if(alturaElemento - 1500 < scrollTop){
+        if(alturaElemento - 2500 < scrollTop){
             animadoAbajo[i].style.opacity = 1;
             animadoAbajo[i].classList.add('desplazarArriba');
         }
